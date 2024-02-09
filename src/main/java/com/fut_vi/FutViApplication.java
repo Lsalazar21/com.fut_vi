e2af5bb83ac4a753386665fe9d8b6c17a7c0dc35
Lsalazar21 <ldiaz40079@ufide.ac.cr>
2024-02-08 23:48:39 -0700

    Tarea2_ViernesIntermedio

---
+++ src/main/java/com/fut_vi/FutViApplication.java
@@ -0,0 +1,13 @@
+package com.fut_vi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutViApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutViApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fut_vi/FutViApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fut_vi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutViApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
